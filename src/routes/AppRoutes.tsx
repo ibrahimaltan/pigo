0626b8aaea04f1c538e11535dc471059f00d9ac2
altanibrahim <ibrahim@madduck.co>
2023-09-05 18:06:21 +0300

    add app routes

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,21 +1,25 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-function Home() {
-    return <div>home</div>;
+import { Link } from 'react-router-dom';
+function Home ()  {
+    return <div>
+        <h1>Home Page</h1>
+        <Link to="/users">Go to User 42 Page</Link>
+    </div>
 }
+function Users () {
+    return <div>
+        <Link to="/"> <h1>Home Page</h1></Link>
+    </div>
 
-function Users() {
-    return <div>users</div>;
 }
-function AppRoutes(props) {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="users" element={<Users />} />
-            </Routes>
-        </BrowserRouter>
-    );
-}
+const AppRoutes: React.FC = () => (
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="users" element={<Users />} />
+        </Routes>
+    </BrowserRouter>
+);
 
-export default AppRoutes;
+export default AppRoutes
